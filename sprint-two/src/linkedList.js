dd5eb918752fb829f35507592f163af0ec023f89
willchen13 <williamchen13225@gmail.com>
2019-12-12 17:50:27 -0800

    finished addToTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,30 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //check is list is empty => list.head=Node(value)
+    if (list.head === null) {
+      list.head = Node(value);
+      list.tail = list[list.head];
+    }
+    //if it exists
+    list.tail = Node(value);
+
+    //if list is not empty => list.tail=Node(value)
   };
 
   list.removeHead = function() {
+    // if (list.head === null) {
+    //   return; // if current head is null then we will just return
+    // }
+    // var oldHead = list.head.value; // save currentHead to a oldHead variable
+    // list.head = list.head.next;
+
+
+    // // oldHead = Node(); // get rid of the old head
+
+    // return oldHead; //return old head
+    list.head = list.head.next.value;
+
   };
 
   list.contains = function(target) {
